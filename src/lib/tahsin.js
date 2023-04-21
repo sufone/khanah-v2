ab55356809a9c651fbf5b99a30a85e4d74a0ebea
Naved Islam <navedcoded@gmail.com>
2023-04-21 20:33:23 +0300

    madd rules

--- src/lib/tahsin.js
+++ src/lib/tahsin.js
@@ -2,11 +2,22 @@ export default function tahsin(inputText) {
 
     if (inputText) {
         let processedText = inputText
+            // <li>حذف الفتحة قبل تاء مربوطة: (قطَة) إلى (قطة)</li>
             .replace(/اً/g, 'ًا')
+
+            // <li>حذف الألف قبل ألف مد: (بَاب) إلى (باب)</li>
             .replace(/َا/g, 'ا')
+
+            // <li>نقل تنوين فتحة من الألف إلى قبلها: (باباً) إلى (بابًا)</li>
             .replace(/َة/g, 'ة')
 
-            // template: .replace(//g, '')
+            // <li>حذف سكون الواو المدية: (طُوْل) إلى (طُول)</li>
+            .replace(/ُوْ/g, 'ُو')
+
+            // <li>حذف سكون الياء المدية: (قِيْل) إلى (قِيل)</li>
+            .replace(/ِيْ/g, 'ِي')
+
+        // template: .replace(//g, '')
 
         return processedText
     }
