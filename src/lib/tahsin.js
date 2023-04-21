4a1611945cb7e81232bcc782d1fe19f532468459
Naved Islam <navedcoded@gmail.com>
2023-04-21 20:24:45 +0300

    add core rules

--- src/lib/tahsin.js
+++ src/lib/tahsin.js
@@ -1,8 +1,12 @@
 export default function tahsin(inputText) {
 
     if (inputText) {
-        let processedText = inputText.replace(/اً/g, 'ًا')
+        let processedText = inputText
+            .replace(/اً/g, 'ًا')
+            .replace(/َا/g, 'ا')
+            .replace(/َة/g, 'ة')
 
+            // template: .replace(//g, '')
 
         return processedText
     }
