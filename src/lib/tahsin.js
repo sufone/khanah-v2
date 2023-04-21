4faa2d91e20140bdb6da8d08b5c93fe1971d1de9
Naved Islam <navedcoded@gmail.com>
2023-04-21 22:49:30 +0300

    convert curly to quranic brace

--- src/lib/tahsin.js
+++ src/lib/tahsin.js
@@ -22,6 +22,10 @@ export default function tahsin(inputText) {
             .replace(/َّ/g, 'َّ')
             .replace(/ِّ/g, 'ِّ')
 
+            // <li>تحويل القوس المعوج إلى قوس قرآني: ({'{'}{'}'}) إلى (﴿﴾)</li>
+            .replace(/{/g, '﴿')
+            .replace(/}/g, '﴾')
+
             
         // template: .replace(//g, '')
 
